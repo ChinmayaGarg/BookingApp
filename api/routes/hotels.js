91c8bc16909300ca7c9db949a03f32900764f961
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-08-20 17:08:43 +0530

    POST request for hotels API (POST will give error)
    POST request handler will create new hotels (document) in "hotels" Collection. The collection name is the name of the endpoint we request from the API.
    POST request raised from client will give error and hence will return STATUS 500 because by default we cannot send any JSON object in request to express server. To prevent it we will use another middleware inside index.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,23 @@
 import express from 'express';
 
+import Hotel from '../models/Hotel.js';
 const router = express.Router();
+
+// CREATE
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  console.log(newHotel, 'newHotel');
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// UPDATE
+// DELETE
+// GET
+// GET ALL
 
 export default router;
