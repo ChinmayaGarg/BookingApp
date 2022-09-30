91281ef0c203ffd81a05d84ef69c43e322595212
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-01 01:07:14 +0530

    Error Handling using Middleware

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -52,7 +52,7 @@ router.get('/', async (req, res) => {
     const hotel = await Hotel.find();
     res.status(200).json(hotel);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 });
 
