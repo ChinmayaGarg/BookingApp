2cf1afc154bad438fbc8c30deb4c5a8f4abcc02c
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-04 13:32:43 +0530

    Minor Fix: illustration purpose, commented code out
    The code was to explain how the verifyToken, verifyUser, verifyAdmin will work. Hence, the code is now commented.

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,17 +4,17 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/checkAuthentication', verifyToken, (req, res, next) => {
-  res.send('Hello user, you are logged in.');
-});
+// router.get('/checkAuthentication', verifyToken, (req, res, next) => {
+//   res.send('Hello user, you are logged in.');
+// });
 
-router.get('/checkUser/:id', verifyUser, (req, res, next) => {
-  res.send('Hello user, you are logged in and you can delete your account.');
-});
+// router.get('/checkUser/:id', verifyUser, (req, res, next) => {
+//   res.send('Hello user, you are logged in and you can delete your account.');
+// });
 
-router.get('/checkAdmin', verifyAdmin, (req, res, next) => {
-  res.send('Hello admin, you are logged in and can delete all accounts.');
-});
+// router.get('/checkAdmin', verifyAdmin, (req, res, next) => {
+//   res.send('Hello admin, you are logged in and can delete all accounts.');
+// });
 
 // UPDATE
 router.put('/:id', updateUser);
