b86f18c032a2a73bf234d1c96baf86c63daf3825
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-11 00:53:17 +0530

    Handled Past date selection
    Now we cannot select dates before today

--- UI/src/components/header/Header.jsx
+++ UI/src/components/header/Header.jsx
@@ -105,6 +105,7 @@ const Header = ({ type }) => {
                     moveRangeOnFirstSelection={false}
                     ranges={date}
                     className="date"
+                    minDate={new Date()}
                   />
                 )}
               </div>
