70e899deb2274c3873bf2d7b17eb667032ac4146
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-11 00:28:32 +0530

    List components created to showcase searched hotels from Home Page

--- UI/src/pages/list/List.jsx
+++ UI/src/pages/list/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import './list.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 
@@ -7,8 +7,26 @@ const List = () => {
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="ls">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label htmlFor="">Destination</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <label htmlFor="">Check-in Date</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default List;
+
+// listSearch = ls
