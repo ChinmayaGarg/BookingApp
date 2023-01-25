66f46247dfccc55141370eea0c82ce99f6871b51
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-01-26 00:46:44 +0530

    Step 3: Created Reducer
    Reducers are basically pure JS functions which take in the previous state and an action and return the newly updated state.
    
    On line 20: const SearchReducer = (state, action) => {
    state is the previous state.
    
    Reducer function will be called by disptch from useReducer.
    
    https://www.freecodecamp.org/news/what-is-redux-store-actions-reducers-explained/#:~:text=Reducers%2C%20as%20the%20name%20suggests,return%20the%20newly%20updated%20state.

--- UI/src/context/SearchContext.js
+++ UI/src/context/SearchContext.js
@@ -1,3 +1,10 @@
+import { createContext, useReducer } from 'react';
+
+export const ACTIONS = {
+  NEW_SEARCH: 'search new hotel',
+  RESET_SEARCH: 'reset search'
+};
+
 const INITIAL_STATE = {
   city: undefined,
   date: [],
@@ -9,3 +16,14 @@ const INITIAL_STATE = {
 };
 
 export const SearchContext = createContext(INITIAL_STATE);
+
+const SearchReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.NEW_SEARCH:
+      return action.payload;
+    case ACTIONS.RESET_SEARCH:
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
