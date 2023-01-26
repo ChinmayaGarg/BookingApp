189660967b6a50e23c34079559c8db01a6b73ac6
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-01-26 19:18:05 +0530

    Navigated to login page on click of login button in NavBar

--- UI/src/components/navbar/Navbar.jsx
+++ UI/src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import './navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
   const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/login');
+  };
 
   return (
     <div className="navbar">
@@ -17,7 +23,9 @@ const Navbar = () => {
         ) : (
           <div className="navItems">
             <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            <button className="navButton" onClick={handleClick}>
+              Login
+            </button>
           </div>
         )}
       </div>
